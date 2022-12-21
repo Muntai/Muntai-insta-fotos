387a801c49c229dd1d07af3c6c489f7be39ffa7d
Мунтай <ruslan.marketing@gmail.com>
2022-12-21 21:40:15 +0200

    settimeout

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 window.onload = function() {
-	Draggable.create('.gallery', {
-		bounds: 'body',
-		inertia: true
-	})
+
+	setTimeout(function() {
+
+		document.body.classList.add('loaded')
+
+			Draggable.create('.gallery', {
+				bounds: 'body',
+				inertia: true
+			})
+			
+		
+
+	}, 200)
+
 }
