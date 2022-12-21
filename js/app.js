b2c0a63cda130feb3f6628bdde13f69fea4e9c73
Мунтай <ruslan.marketing@gmail.com>
2022-12-22 00:05:09 +0200

    add mobile

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-window.onload = function() {
-
-	setTimeout(function() {
-
-		document.body.classList.add('loaded')
-
-			Draggable.create('.gallery', {
+window.onload = function() { // После того, как окно загружено
+ 
+	setTimeout(function() { // Устанавливаем таймаут на 200мс
+ 
+		document.body.classList.add('loaded') // Добавляем класс готовности body
+ 
+		if (window.matchMedia('(min-width: 992px)').matches) { // Если не смартфон
+ 
+			Draggable.create('.gallery', { // Запускаем плагин Draggable
 				bounds: 'body',
 				inertia: true
 			})
 			
-		
+		}
+ 
 	}, 200)
-
+ 
 }
